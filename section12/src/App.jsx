2dd78cd9c68e86051cd12671d6a26dc75606b1bc
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-08-07 15:00:08 +0000

    commit

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -3,52 +3,97 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from './pages/Edit';
 import Notfound from "./pages/Notfound";
-import Button from './components/Button';
-import Header from './components/Header';
+import { useReducer, useRef } from 'react';
+
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "Update":
+      return state.map((item) => 
+        String(item.id) === String(action.data.id)
+         ? action.data 
+         : item
+      );
+    case "Delete":
+      return state.filter(
+        (item) => String(item.id) !== String(action.data.id)
+      );
+    default :
+      return state;
+  }
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "Update",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "Delete",
+      id,
+    })
+  }
 
   return (
     <>
-      <Header 
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      
-      <Button 
-        text={"123"} 
-        type={"DEFAULT"}
-        onClick={() => {
-        console.log("123버튼 클릭!");
-      }}
-      />
+    <button onClick={()=>{
+      onCreate(new Date().getTime(), 1, "일기 3번")
+    }}>일기 추가 테스트</button>
 
-      <Button 
-        text={"123"} 
-        type={"POSITIVE"}
-        onClick={() => {
-        console.log("123버튼 클릭!");
-      }}
-      />
-
-      <Button 
-        text={"123"} 
-        type={"NEGATIVE"}
-        onClick={() => {
-        console.log("123버튼 클릭!");
-      }}
-      />
-
+    <button onClick={()=>{
+      onUpdate(1, new Date().getTime(), 3, "일기 수정")
+    }}>일기 수정 테스트</button>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/new" element={<New />}/>
         <Route path="/diary/:id" element={<Diary />}/>
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ section12/src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const Edit = () => {
+    const params = useParams();
+
+    return (
+        <div>
+            {params.id}
+        </div>
+    );
+};
+
+export default Edit;
