d82e09a85bbc1f269556d6405bc8b92bd2d2f748
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-08-09 14:59:49 +0000

    commit

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -10,14 +10,20 @@ import { useReducer, useRef, createContext } from 'react';
 const mockData = [
   {
     id: 1,
-    createDate: new Date().getTime(),
+    createDate: new Date("2024-08-19").getTime(),
     emotionId: 1,
     content: "1번 일기",
   },
   {
     id: 2,
-    createDate: new Date().getTime(),
+    createDate: new Date("2024-08-18").getTime(),
     emotionId: 2,
+    content: "2번 일기",
+  },
+  {
+    id: 3,
+    createDate: new Date("2024-07-18").getTime(),
+    emotionId: 3,
     content: "2번 일기",
   },
 ];
@@ -41,8 +47,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,14 +1,59 @@
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList"
 
+const getMonthlyData = (pivotDate, data) => {
+    const beginTime = new Date(
+        pivotDate.getFullYear(),
+        pivotDate.getMonth(),
+        1,
+        0,
+        0,
+        0
+    ).getTime();
+
+    const endTime = new Date(
+        pivotDate.getFullYear(),
+        pivotDate.getMonth() + 1,
+        0,
+        23,
+        59,
+        59,
+    ).getTime();
+
+    return data.filter((item) => 
+        item.createDate >= beginTime && item.createDate <= endTime
+    )
+}
+
 const Home = () => {
+    const data = useContext(DiaryStateContext);
+
+    const monthlyData = getMonthlyData(pivotDate, data);
+    console.log(monthlyData);
+
+    const [pivotDate, setPivotDate] = useState(new Date());
+
+    const onIncreaseMonth = () => {
+        setPivotDate(
+            new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
+        );
+    };
+    const onDecreaseMonth = () => {
+        setPivotDate(
+            new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
+        );
+    };
+
     return (
         <div>
             <Header
-                title={"2024년 2월"} 
-                leftChild={<Button text={"<"}/>} 
-                rightChild={<Button text={">"}/>}
+                title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} 
+                leftChild={<Button onClick={onDecreaseMonth} text={"<"}/>} 
+                rightChild={<Button onClick={onIncreaseMonth} text={">"}/>}
             />
             <DiaryList />
         </div>
