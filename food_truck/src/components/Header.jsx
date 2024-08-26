5dc2c73ecfd8a9ae6668041cb1f03f1ee60c3a0a
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-08-26 14:56:41 +0000

    commit

--- food_truck/src/components/Header.jsx
+++ food_truck/src/components/Header.jsx
@@ -3,31 +3,32 @@ import brand_logo from '../assets/brand_logo.png';
 import instagram_logo from '../assets/instagram_logo.png';
 import naverblog_logo from '../assets/naverblog_logo.png';
 import youtube_logo from '../assets/youtube_logo.png';
-import Menu from './Menu'
+import Menu from './Menu';
+import Search from './Search';
 
 const Header = () => {
     return(
         <header id="header">
-            <div className="brand_header">
-                <img src={brand_logo} />
-            </div>
-            <div className="navbar-menu">
-                <div className="navbar-social-media">
-                    <img src={instagram_logo} onClick={()=>{window.open("https://www.instagram.com/")}} />
-                    <img src={naverblog_logo} onClick={()=>{window.open("https://www.naver.com/")}} />
-                    <img src={youtube_logo} onClick={()=>{window.open("https://www.youtube.com/")}} />
+            <div className="header_class">
+                <div className="brand_header">
+                    <img src={brand_logo} />
                 </div>
-                <div className="navbar-right">
-                    <Menu />
-                    <div className="nabar-search">
-                        검색바
+                <div className="navbar-menu">
+                    <div className="navbar-social-media">
+                        <img src={instagram_logo} onClick={()=>{window.open("https://www.instagram.com/")}} />
+                        <img src={naverblog_logo} onClick={()=>{window.open("https://www.naver.com/")}} />
+                        <img src={youtube_logo} onClick={()=>{window.open("https://www.youtube.com/")}} />
                     </div>
-                    <div className="navbar-auth">   
-                        <div className="button">
-                            로그인
-                        </div>
-                        <div className="button">
-                            회원가입
+                    <div className="navbar-right">
+                        <Menu />
+                        <Search />
+                        <div className="navbar-auth">   
+                            <div className="button">
+                                로그인
+                            </div>
+                            <div className="button">
+                                회원가입
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ food_truck/src/components/Search.jsx
@@ -0,0 +1,9 @@
+import "./Search.css"
+
+const Search = () => {
+    return(
+        <div>검색바</div>
+    );
+};
+
+export default Search;
