ee69250c9a21e40b0dce4490ea42f714da5bf925
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-09-20 14:53:32 +0000

    commit

--- food_truck/src/components/LoginModal.jsx
+++ food_truck/src/components/LoginModal.jsx
@@ -3,8 +3,10 @@ import './LoginModal.css';
 const LoginModal = ({ isOpen, closeModal}) => {
     return(
         <div style={{display: isOpen ? "block" : "none"}}>
-            <div className='login_modal-window'>
-                모달입니다.
+            <div className='login_modal-overlay'>
+                <div className='login_modal-window'>
+                    모달입니다.
+                </div>
             </div>
         </div>
     );
