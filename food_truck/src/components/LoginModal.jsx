afb63c29e6ea002961abaff30d503e2061b5d1d6
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-09-21 14:51:37 +0000

    commit

--- food_truck/src/components/LoginModal.jsx
+++ food_truck/src/components/LoginModal.jsx
@@ -1,11 +1,34 @@
 import './LoginModal.css';
+import brand_logo from '../assets/brand_logo.png';
+import { useRef } from 'react';
+import { FcGoogle } from "react-icons/fc";
+
 
 const LoginModal = ({ isOpen, closeModal}) => {
+    const modalBackground = useRef();
+
     return(
         <div style={{display: isOpen ? "block" : "none"}}>
-            <div className='login_modal-overlay'>
+            <div className='login_modal-overlay' ref={modalBackground} onClick={e => {
+                if(e.target === modalBackground.current){
+                    closeModal();
+                }
+            }}
+            >
                 <div className='login_modal-window'>
-                    모달입니다.
+                    <img src={brand_logo} />
+                    <form action="" method='post'>
+                        <input type="text" placeholder="  이메일" />
+                        <input type="text" placeholder="  비밀번호"/>
+                        <button>로그인</button>
+                    </form>
+                    <div className='login_modal-singIn-modal'>
+                        <a href='id'>아이디(이메일) 찾기</a>
+                        <span>|</span>
+                        <a href='password'>비밀번호 찾기</a>
+                        <span>|</span>
+                        <a href='register'>회원가입</a>
+                    </div>
                 </div>
             </div>
         </div>
