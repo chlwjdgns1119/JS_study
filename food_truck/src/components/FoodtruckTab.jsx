257b4168bd5abf1c845304fe8fc054a624ce57b4
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-10-07 15:00:39 +0000

    commit

---
+++ food_truck/src/components/FoodtruckTab.jsx
@@ -0,0 +1,49 @@
+import './FoodtruckTab.css';
+
+const FoodtruckTab = () => {
+    return(
+        <div className="foodtrucktab-list">
+            <div className="foodtrucktab-item">
+                전체
+            </div>
+            <div className="foodtrucktab-item">
+                식사류
+            </div>
+            <div className="foodtrucktab-item">
+                음료류
+            </div>
+            <div className="foodtrucktab-item">
+                면류
+            </div>
+            <div className="foodtrucktab-item">
+                덮밥류
+            </div>
+            <div className="foodtrucktab-item">
+                분식류
+            </div>
+            <div className="foodtrucktab-item">
+                양식
+            </div>
+            <div className="foodtrucktab-item">
+                중식
+            </div>
+            <div className="foodtrucktab-item">
+                일식
+            </div>
+            <div className="foodtrucktab-item">
+                한식
+            </div>
+            <div className="foodtrucktab-item">
+                도시락  
+            </div>
+            <div className="foodtrucktab-item">
+                간식 
+            </div>
+            <div className="foodtrucktab-item">
+                디저트류 
+            </div>
+        </div>
+    )
+}
+
+export default FoodtruckTab;
