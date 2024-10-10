907f76284ce69c646215e37ec5e1d4417afae2b1
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-10-10 14:46:56 +0000

    commit

--- food_truck/src/components/FoodtruckTab.jsx
+++ food_truck/src/components/FoodtruckTab.jsx
@@ -3,8 +3,14 @@ import { MdOutlineFoodBank } from "react-icons/md";
 import { RiDrinks2Line } from "react-icons/ri";
 import { GiNoodles } from "react-icons/gi";
 import { PiBowlFood } from "react-icons/pi";
-
-
+import { PiHamburgerFill } from "react-icons/pi";
+import { GiDumpling } from "react-icons/gi";
+import { GiSushis } from "react-icons/gi";
+import tteokbokki from "../assets/tteokbokki.png";
+import bibimbap from "../assets/bibimbap.png";
+import lunchbox from "../assets/lunchbox.png";
+import takoyaki from "../assets/takoyaki.png";
+import shavedice from "../assets/shavedice.png";
 
 const FoodtruckTab = () => {
     return(
@@ -28,28 +34,36 @@ const FoodtruckTab = () => {
                 <PiBowlFood className='bowls-icon' />
                 덮밥류
             </div>
-            <div className="foodtrucktab-item">
-                분식류
+            <div className="foodtrucktab-item">  
+                <img src={tteokbokki} className='tteokbokki-icon'/>
+                떡볶이
             </div>
             <div className="foodtrucktab-item">
+                <PiHamburgerFill className='pizza-icon'/>
                 양식
             </div>
             <div className="foodtrucktab-item">
+                <GiDumpling className='dumpling-icon'/>
                 중식
             </div>
             <div className="foodtrucktab-item">
+                <GiSushis className='sushi-icon'/>
                 일식
             </div>
             <div className="foodtrucktab-item">
+                <img src={bibimbap} className='bibimbap-icon'/>
                 한식
             </div>
             <div className="foodtrucktab-item">
+                <img src={lunchbox} className='lunchbox-icon'/>
                 도시락  
             </div>
             <div className="foodtrucktab-item">
+                <img src={takoyaki} className='takoyaki-icon'/>
                 간식 
             </div>
             <div className="foodtrucktab-item">
+                <img src={shavedice}  className='shavedice-icon'/>
                 디저트류 
             </div>
         </div>
