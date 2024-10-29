c2e0eae17a2553db892484bf680165f88d2f8b67
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-10-29 14:47:46 +0000

    commit

--- food_truck/src/components/TentstallMain.jsx
+++ food_truck/src/components/TentstallMain.jsx
@@ -1,44 +1,75 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { FaCheck } from "react-icons/fa6";
 import TentstallItemlist from './TentstallItemlist';
 import './TentstallMain.css';
 
 const TentstallMain = () => {
 
+    const [rent_toggle, setRentToggle] = useState(false);
+    const [rent_state, setRentState] = useState('전체');
     const [sort_checked, setSortchecked] = useState(1);
+
+    const divRef = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (divRef.current && !divRef.current.contains(event.target)) {
+            setRentToggle(false);
+        }
+    };
+
+    const handleClickInside = (event) => {
+        event.stopPropagation();
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return(
         <div className='tentstall-main'>
             <div className='tentstall-main-filter'>
-                <div className='tentstall-main-filter-rent'>
-                    
+                <div className='tentstall-main-filter-rent' onClick={()=>{(rent_toggle ? setRentToggle(false): setRentToggle(true))}}  ref={divRef}>
+                   <div className='filter-state'>
+                        {rent_state}
+                    </div>
+                    {rent_toggle && (
+                        <div className='filter-dropdown'>
+                            <div>전체</div>
+                            <div>구매</div>
+                            <div>대여</div>
+                        </div>
+                    )}
                 </div>
                 <div className='tentstall-main-filter-sort'>
                     <div 
                         className={`${(sort_checked===1) ? `sort-hovered` : ""}`}
                         onClick={()=>{setSortchecked(1)}}
                     >
-                        {(sort_checked===1)&&`c`}추천순
+                        {(sort_checked===1)&&<FaCheck className='tentstall-main-checkicon'/>}추천순
                     </div>
                     <span>|</span>
                     <div
                         className={`${(sort_checked===2) ? `sort-hovered` : ""}`}
                         onClick={()=>{setSortchecked(2)}}
                     >
-                        인기순
+                        {(sort_checked===2)&&<FaCheck className='tentstall-main-checkicon'/>}인기순
                     </div>
                     <span>|</span>
                     <div 
                         className={`${(sort_checked===3) ? `sort-hovered` : ""}`}
                         onClick={()=>{setSortchecked(3)}}
                     >
-                        평점순
+                        {(sort_checked===3)&&<FaCheck className='tentstall-main-checkicon'/>}평점순
                     </div>
                     <span>|</span>
                     <div 
                         className={`${(sort_checked===4) ? `sort-hovered` : ""}`}
                         onClick={()=>{setSortchecked(4)}}
                     >
-                        최신순
+                        {(sort_checked===4)&&<FaCheck className='tentstall-main-checkicon'/>}최신순
                     </div>
                 </div>
             </div>
