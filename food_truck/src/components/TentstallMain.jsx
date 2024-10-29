b8dc82f5dd38a781f420f18fdf1aa024689ea311
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-10-29 11:53:39 +0000

    commit

--- food_truck/src/components/TentstallMain.jsx
+++ food_truck/src/components/TentstallMain.jsx
@@ -1,19 +1,45 @@
+import { useState } from 'react';
 import TentstallItemlist from './TentstallItemlist';
 import './TentstallMain.css';
 
 const TentstallMain = () => {
+
+    const [sort_checked, setSortchecked] = useState(1);
+
     return(
         <div className='tentstall-main'>
             <div className='tentstall-main-filter'>
                 <div className='tentstall-main-filter-rent'>
-                    <div>추천순</div>
-                    <span>|</span>
-                    <div>인기순</div>
-                    <div>평점순</div>
-                    <div>최신순</div>
+                    
                 </div>
                 <div className='tentstall-main-filter-sort'>
-
+                    <div 
+                        className={`${(sort_checked===1) ? `sort-hovered` : ""}`}
+                        onClick={()=>{setSortchecked(1)}}
+                    >
+                        {(sort_checked===1)&&`c`}추천순
+                    </div>
+                    <span>|</span>
+                    <div
+                        className={`${(sort_checked===2) ? `sort-hovered` : ""}`}
+                        onClick={()=>{setSortchecked(2)}}
+                    >
+                        인기순
+                    </div>
+                    <span>|</span>
+                    <div 
+                        className={`${(sort_checked===3) ? `sort-hovered` : ""}`}
+                        onClick={()=>{setSortchecked(3)}}
+                    >
+                        평점순
+                    </div>
+                    <span>|</span>
+                    <div 
+                        className={`${(sort_checked===4) ? `sort-hovered` : ""}`}
+                        onClick={()=>{setSortchecked(4)}}
+                    >
+                        최신순
+                    </div>
                 </div>
             </div>
             <div className='tentstall-main-itemlist'>
