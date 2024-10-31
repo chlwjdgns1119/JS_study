0542546298553fe8e0e585f1ba4a06f5a51879d3
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-10-31 14:39:27 +0000

    commit

--- food_truck/src/components/TentstallItem.jsx
+++ food_truck/src/components/TentstallItem.jsx
@@ -1,17 +1,20 @@
 import './TentstallItem.css';
 import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
-import example from '../assets/example.png';
+import example from '../assets/tentstallItem_example.png';
+import { useState } from 'react';
 
 const TentstallItem = ({score, reviewer}) => {
+    const [itemHover, setItemHover] = useState(false);
+
     return(
-        <div className='tentstall-item'>
-            <img />
+        <div className={`tentstall-item ${itemHover ? `tentstall-item-hovered` : ""}`} onMouseOver={()=>{setItemHover(true)}} onMouseOut={()=>{setItemHover(false)}}>
+            <img src={example}/>
             <div className='tentstall-item-explain'>
                 <div className='tentstall-item-title'>
-                    푸드트럭 아이템 제목
+                    40구 타코야끼 기계
                 </div>
                 <div className='tentstall-item-price'>
-                    푸드트럭 아이템 가격
+                    17,800원
                 </div>
                 <div className='tentstall-item-score-rating'>
                     {Array.from({ length: score-1 }, (_, index) => (
--- food_truck/src/components/TentstallItemlist.jsx
+++ food_truck/src/components/TentstallItemlist.jsx
@@ -2,9 +2,18 @@ import './TentstallItemlist.css';
 import TentstallItem from './TentstallItem';
 
 const TentstallItemlist = () => {
+
     return(
-        <div>
-            <TentstallItem score={4.4} reviewer={189}/>
+        <div className='tentstall-itemlist'>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
+            <TentstallItem className='tentstall-itemlist-container' score={4.4} reviewer={189}/>
         </div>
     );
 };
