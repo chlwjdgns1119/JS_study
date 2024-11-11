77baf1b369d775285696137c787a3f0915ac18b2
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-11-11 07:25:44 +0000

    commit

--- food_truck/src/components/TestEditorForm.jsx
+++ food_truck/src/components/TestEditorForm.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import { convertToRaw, convertFromRaw, Editor, EditorState, RichUtils } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 import 'draft-js/dist/Draft.css';
 
 const TestEditorForm = () => {
   const [editorState, setEditorState] = useState(()=>
     EditorState.createEmpty()
   );
+
+  const htmlCode = draftToHtml(JSON.parse(localStorage.getItem("my-draft")));
 
   useEffect(() => {
     const raw = localStorage.getItem("my-draft");
@@ -26,14 +29,20 @@ const TestEditorForm = () => {
     const contentState = editorState.getCurrentContent();
     const raw = convertToRaw(contentState);
     console.log(raw);
-    localStorage.setItem('my-draft', JSON.stringify(raw, null, 2))
+    localStorage.setItem("my-draft", JSON.stringify(raw, null, 2))
+  }
+
+  const getStateByHtml = () => {
+    console.log(htmlCode);
   }
 
   return (
     <div>
       <button onClick={saveContent}>저장입니다</button>
       <button onClick={toggleBold}>BOLD</button>
+      <button onClick={getStateByHtml}>html코드 출력</button>
       <Editor editorState={editorState} onChange={setEditorState} />
+      <div>{htmlCode}</div>
     </div>
   )
 };
