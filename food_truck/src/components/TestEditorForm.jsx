3b2672cdb81664793e95a4e56f36dc5e87bfdb88
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2024-11-30 11:28:06 +0000

    commit

--- food_truck/src/components/TestEditorForm.jsx
+++ food_truck/src/components/TestEditorForm.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
-import { convertToRaw, convertFromRaw, Editor, EditorState, RichUtils } from 'draft-js';
+import { convertToRaw, convertFromRaw, EditorState, RichUtils } from 'draft-js';
+
+import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from 'draftjs-to-html';
 import DOMPurify from 'dompurify';
 import 'draft-js/dist/Draft.css';
+import './TestEditorForm.css';
 
 const TestEditorForm = () => {
   const [editorState, setEditorState] = useState(()=>
@@ -37,15 +40,37 @@ const TestEditorForm = () => {
     console.log(htmlCode);
   }
 
+  const toolbar = {
+    list: { inDropdown: true }, // list 드롭다운
+    textAlign: { inDropdown: true }, // align 드롭다운
+    link: { inDropdown: true }, // link 드롭다운
+    history: { inDropdown: false }, // history 드롭다운 // 이미지 커스텀 업로드
+}
+
   const sanitizedContent = DOMPurify.sanitize(htmlCode);
 
   return (
     <div>
-      <button onClick={saveContent}>저장입니다</button>
-      <button onClick={toggleBold}>BOLD</button>
-      <button onClick={getStateByHtml}>html코드 출력 {console.log(htmlCode)}</button>
-      <Editor editorState={editorState} onChange={setEditorState} />
-      <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      <div>
+        <button onClick={saveContent}>저장입니다</button>
+        <button onClick={toggleBold}>BOLD</button>
+        <button onClick={getStateByHtml}>html코드 출력 {console.log(htmlCode)}</button>
+        <Editor
+          editorClassName="editor"
+          toolbarClassName="toolbar-class"
+          toolbar={toolbar} 
+          placeholder="내용을 작성해주세요."
+          // 한국어 설정
+          localization={{
+              locale: 'ko',
+          }}
+          editorState={editorState} 
+          onChange={setEditorState} 
+        />
+        <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+        
+      </div>
+      <Editor editorClassName="loaded_state" editorState={editorState} />
     </div>
   )
 };
