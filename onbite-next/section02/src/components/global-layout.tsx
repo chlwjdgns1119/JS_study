74a25e8fcc36d7ee84ed046c0aba78603fae7547
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2025-02-16 15:00:14 +0000

    commit

--- onbite-next/section02/src/components/global-layout.tsx
+++ onbite-next/section02/src/components/global-layout.tsx
@@ -1,9 +1,16 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import style from "./global-layout.module.css";
+
 export default function GlobalLayout({children}: {
     children: ReactNode
 }) {
-    <div>
-        <header>헤더</header>
-        <main>{children}</main>
-        <footer>푸터</footer>
-      </div>
+    return(
+        <div className={style.container}>
+            <header className={style.header}>헤더</header>
+                <Link href={"/"}>Onbitebooks</Link>
+            <main className={style.main}>{children}</main>
+            <footer className={style.footer}>제작 @chlwjdgns915</footer>
+        </div>
+    )
 }
