6bf8445d0cbdafda914d5e18829823b4c839dd60
chlwjdgns1119 <155228342+chlwjdgns1119@users.noreply.github.com>
2025-03-27 14:51:57 +0000

    commit

--- onbite-next/section02/src/pages/search/index.tsx
+++ onbite-next/section02/src/pages/search/index.tsx
@@ -1,11 +1,13 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetServerSidePropsContext, GetStaticProps, GetStaticPropsContext, InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
+/* export const getStaticProps = async (
+  context: GetStaticPropsContext
 ) => {
   const q = context.query.q;
   const books = await fetchBooks(q as string);
@@ -15,9 +17,25 @@ export const getServerSideProps = async (
       books,
     },
   };
-};
+}; */
 
-export default function Page({books}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  };
+
+  useEffect(()=>{
+    if(q){
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
